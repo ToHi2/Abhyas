1c9bc6f703e3df9ab3819cceb3e7872922b86d26
ToHi2 <114604305+ToHi2@users.noreply.github.com>
2024-12-30 21:52:18 +0530

    Update script.js

--- script.js
+++ script.js
@@ -12,4 +12,21 @@ function loadFunc(){
     if( n != null && n != undefined && n != NaN){ 
       document.getElementById("sigma").innerHTML = localStorage.saveList; 
     } 
+ function dayStreak(){
+   var date = new Date();
+   var day = date.getDate();
+   if(localStorage.dateStor != day){
+    numMod('day',1,'add');
+   }
+ }
+ function numMod(id, val, type) { 
+   switch (type) { 
+     case "add": 
+       document.getElementById(id).innerHTML = parseInt(document.getElementById(id).innerHTML) + val; 
+       break; 
+     case "sub": 
+       document.getElementById(id).innerHTML = parseInt(document.getElementById(id).innerHTML) - val; 
+       break; 
+   } 
+       }
  }
